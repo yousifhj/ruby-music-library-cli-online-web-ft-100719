0ea88a71358cd0988621fbbcad37cceb2799842d
Huda Yousif <hjdoski@gmail.com>
2019-10-24 22:52:29 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,7 +16,7 @@ class MusicLibraryController
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
-    
+    puts "To quit, type 'exit'."
   else 
   
   end
