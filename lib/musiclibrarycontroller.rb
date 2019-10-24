c4e95924f41c86f21384cacd374e84bd5be8fa78
Huda Yousif <hjdoski@gmail.com>
2019-10-24 22:51:20 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -13,6 +13,7 @@ class MusicLibraryController
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
   else 
   
   end
