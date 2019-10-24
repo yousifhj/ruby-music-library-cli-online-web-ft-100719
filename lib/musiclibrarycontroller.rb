8870b0b956f5ced65f9e9c98e611d987c869abf2
Huda Yousif <hjdoski@gmail.com>
2019-10-24 23:07:45 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,6 +23,7 @@ class MusicLibraryController
 
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, x|
+      puts "#{x}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end 
 end 
  
