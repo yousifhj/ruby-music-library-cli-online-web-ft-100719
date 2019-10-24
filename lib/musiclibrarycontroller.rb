d35fc63058235433a6db128263a921f88f8196c2
Huda Yousif <hjdoski@gmail.com>
2019-10-24 22:48:31 +0000

    Automatically backed up by Learn

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,19 @@
+class MusicLibraryController 
+  
+  def initialize(path = './db/mp3s')
+    importer = MusicImporter.new(path)
+    importer.import
+  end
+
+  def call 
+    input = ''
+    
+    while input != 'exit'
+    puts "Welcome to your music library!"
+
+  end
+  
+    
+ 
+
+end 
