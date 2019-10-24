afb312441d54e60da34d85fe66b19d07c17db1b3
Huda Yousif <hjdoski@gmail.com>
2019-10-24 23:14:13 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -38,7 +38,7 @@ class MusicLibraryController
   end 
   
   def list_songs_by_artist
-    puts "Please enter the name iof the artist:"
+    puts "Please enter the name of the artist:"
   end
   
 end 
