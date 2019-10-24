912023a6a86a5a829d3a157dcc064d34469dc2b3
Huda Yousif <hjdoski@gmail.com>
2019-10-24 23:02:34 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -19,12 +19,6 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip
-
-
-  case input 
-  when 'list songs'
-    self.list_songs
-  end 
  
   end 
 
